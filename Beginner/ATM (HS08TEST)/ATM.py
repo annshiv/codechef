e93b6769a9374ab7d3b1629cbae4eccf4fc9f99a
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-03-27 17:55:31 +0530

    Update ATM.py

--- Beginner/ATM (HS08TEST)/ATM.py
+++ Beginner/ATM (HS08TEST)/ATM.py
@@ -6,3 +6,11 @@ if (w % 5 == 0 and b>=(w+0.5)):
     print('%.2f' % b)
 else:
     print('%.2f' % b)
+    
+    
+#in a different style
+withdraw, bal = map(float,  input().split() )
+
+if withdraw%5==0 and withdraw+0.50<=bal:
+    bal-= (withdraw+0.50)
+print(bal)

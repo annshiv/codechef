88ddf7291800416bc07f53d162b6404cd8dbc493
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-03-28 16:54:41 +0530

    Update Add_two_numbers.py

--- Beginner/Add two numbers (FLOW001)/Add_two_numbers.py
+++ Beginner/Add two numbers (FLOW001)/Add_two_numbers.py
@@ -1,3 +1,9 @@
 for _ in range(int(input())):
     a,b = map(int,input().split(' '))
     print(a+b)
+    
+    
+#using list
+for _ in range(int(input())):
+    l=list(map(int, input().split()))
+    print(sum(l))

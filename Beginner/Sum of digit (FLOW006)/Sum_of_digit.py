abcb11384f814d9509bf96a96bbf78048ef17455
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-03-30 10:34:52 +0530

    Update Sum_of_digit.py

--- Beginner/Sum of digit (FLOW006)/Sum_of_digit.py
+++ Beginner/Sum of digit (FLOW006)/Sum_of_digit.py
@@ -1,3 +1,12 @@
 for _ in range(int(input())):
     a = list(map(int,input().strip()))
     print(sum(a))
+    
+    
+#using 'for' loop
+for _ in range(int(input())):
+    res=0
+    for i in input():
+        res+=int(i)
+    print(res)
+    

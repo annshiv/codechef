ac3b6cd60d0063f447e0fcc5c539b5993731e9fa
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-04-08 08:24:29 +0530

    Update First_and_last_digit.py

--- Beginner/First and last digit (FLOW002)/First_and_last_digit.py
+++ Beginner/First and last digit (FLOW002)/First_and_last_digit.py
@@ -1,3 +1,9 @@
 for _ in range(int(input())):
     a = list(map(int,input().strip()))
     print(a[0]+a[-1])
+    
+
+#Time: 0.01 sec
+for _ in range(int(input())):
+    s=input()
+    print(int(s[0])+int(s[-1]))

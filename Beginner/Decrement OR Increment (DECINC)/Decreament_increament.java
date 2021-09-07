7fac873a740829aa94424485153e8c66989b6779
Murali03M <murali732000@gmail.com>
2021-09-07 22:40:09 +0530

    decreament or increament

---
+++ Beginner/Decrement OR Increment (DECINC)/Decreament_increament.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Decreament_increament
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		if(num%4==0)
+		{
+		    System.out.println(num+1);
+		}
+		else
+		{
+		    System.out.println(num-1);
+		}
+	}
+}

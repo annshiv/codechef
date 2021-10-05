1485cbbfc717dbb4d611d8a426f36e45517ff4d5
Logesh P <pjlogeshnila@gmail.com>
2021-10-05 22:25:52 +0530

    change made

--- Beginner/ATM (HS08TEST)/ATM.java
+++ Beginner/ATM (HS08TEST)/ATM.java
@@ -5,6 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
+//changes done
 class Codechef
 {
 	public static void main (String[] args)
